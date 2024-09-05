eac2f0a776840f0a4c237468c4d9c2e1dc4b0a5b
DrumKit <sumitbhargav12@gmail.com>
2024-09-05 17:46:32 +0530

    Delete Api Element

--- Data/MakeListCallAPI.js
+++ Data/MakeListCallAPI.js
@@ -13,6 +13,20 @@ const MakeListCallAPI = () => {
       setData(result);
     }
   };
+
+  const deleteUser = async (id) => {
+    const url = 'http://192.168.1.4:3000/users';
+    // console.warn(`${url}/${id}`);
+    let result = await fetch(`${url}/${id}`, {
+      method: 'delete',
+    });
+    result = await result.json();
+    if (result) {
+      console.warn('User deleted');
+      getAPIData();
+    }
+  };
+
   useEffect(() => {
     getAPIData();
   }, []);
@@ -20,19 +34,32 @@ const MakeListCallAPI = () => {
   return (
     <ScrollView style={styles.input}>
       <View style={styles.errorText}>
-                <View style={{flex:1.5}}><Text>Name</Text></View>
-                <View style={{flex:1.5}} ><Text>Id</Text></View>
-                <View style={{flex:2}}><Text>Operations</Text></View>
-            </View>   
+        <View style={{flex: 1.5}}>
+          <Text>Name</Text>
+        </View>
+        <View style={{flex: 1.5}}>
+          <Text>Id</Text>
+        </View>
+        <View style={{flex: 2}}>
+          <Text>Operations</Text>
+        </View>
+      </View>
       {data.length
         ? data.map(item => (
             <View style={styles.errorText}>
-                <View style={{flex:1}}><Text>{item.id}</Text></View>
-                <View style={{flex:1}} ><Text>{item.name}</Text></View>
-                <View style={{flex:1}}><Button title='Delete'/></View>
-                <View style={{flex:1}}><Button title='Update' /></View>
+              <View style={{flex: 1}}>
+                <Text>{item.id}</Text>
+              </View>
+              <View style={{flex: 1}}>
+                <Text>{item.name}</Text>
+              </View>
+              <View style={{flex: 1}}>
+                <Button onPress={()=>deleteUser(item.id)} title="Delete" />
+              </View>
+              <View style={{flex: 1}}>
+                <Button title="Update" />
+              </View>
             </View>
-            
           ))
         : null}
     </ScrollView>
@@ -40,16 +67,12 @@ const MakeListCallAPI = () => {
 };
 
 const styles = StyleSheet.create({
-  input: {
-    
-  },
+  input: {},
   errorText: {
-    flexDirection:"row",
-    justifyContent:'space-between',
-    backgroundColor:'orange',
-    margin:5
-    
-   
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'orange',
+    margin: 5,
   },
 });
 
